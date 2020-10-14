0ff98511c0cb52170be42dee3f3d539afaa6de6a
flowtoolz <contact@flowtoolz.com>
2020-10-14 13:16:16 +0200

    Proof concept: Continuously process console input while running

--- Sources/LanguageService/configure.swift
+++ Sources/LanguageService/configure.swift
@@ -8,4 +8,32 @@ public func configure(_ app: Application) throws {
     app.http.server.configuration.serverName = "Language Service Host"
     
     try registerRoutes(on: app)
+    
+    startProcessingConsoleInput(app: app)
+}
+
+func startProcessingConsoleInput(app: Application) {
+    app.console.output("👋🏻 Type in commands to configure the language service ⌨️ ...")
+    processNextConsoleInput(app: app)
+}
+
+func processNextConsoleInput(app: Application) {
+    let eventLoop = app.eventLoopGroup.next()
+    
+    let didReadConsole = app.threadPool.runIfActive(eventLoop: eventLoop) {
+        app.console.input()
+    }
+    
+    didReadConsole.whenSuccess { input in
+        process(input: input, from: app.console)
+        processNextConsoleInput(app: app)
+    }
+    
+    didReadConsole.whenFailure { error in
+        app.console.output("Error: \(error.localizedDescription)".consoleText())
+    }
+}
+
+func process(input: String, from console: Console) {
+    console.output("You typed \(input)".consoleText())
 }
