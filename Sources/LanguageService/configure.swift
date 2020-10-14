4feac7a8e2cc72507a551c9d98044f879e8bdf88
flowtoolz <contact@flowtoolz.com>
2020-10-14 14:54:15 +0200

    Control log level programmatically

--- Sources/LanguageService/configure.swift
+++ Sources/LanguageService/configure.swift
@@ -5,6 +5,8 @@ public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
     
+    app.logger.logLevel = .warning
+    
     app.http.server.configuration.serverName = "Language Service Host"
     
     try registerRoutes(on: app)
