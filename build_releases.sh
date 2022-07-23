3c508e4298233e4d388bbac9d30171c761c2dcef
Sebastian Fichtner <contact@flowtoolz.com>
2022-07-23 14:21:06 +0200

    Move binaries to dedicated web page

--- build_releases.sh
+++ build_releases.sh
@@ -1,5 +1,5 @@
 swift build --configuration release --arch arm64
-cp .build/arm64-apple-macosx/release/LSPService Binaries/arm64-apple-macosx/LSPService
+cp .build/arm64-apple-macosx/release/LSPService ../flowtoolz.github.io/codeface/lspservice/binaries/arm64-apple-macosx/LSPService
 
 swift build --configuration release --arch x86_64
-cp .build/x86_64-apple-macosx/release/LSPService Binaries/x86_64-apple-macosx/LSPService
+cp .build/x86_64-apple-macosx/release/LSPService ../flowtoolz.github.io/codeface/lspservice/binaries/x86_64-apple-macosx/LSPService
