81bf16967bdb6c77263d3e762ed922c2272f941e
Sebastian Fichtner <contact@flowtoolz.com>
2022-07-23 16:16:40 +0200

    Junk directory paths when zipping binaries
    https://linux.die.net/man/1/zip

--- build_releases.sh
+++ build_releases.sh
@@ -1,5 +1,6 @@
 swift build --configuration release --arch arm64
-zip ../flowtoolz.github.io/codeface/lspservice/binaries/arm64-apple-macosx/LSPService.zip .build/arm64-apple-macosx/release/LSPService
+zip -j ../flowtoolz.github.io/codeface/lspservice/binaries/arm64-apple-macosx/LSPService.zip .build/arm64-apple-macosx/release/LSPService
+
 
 swift build --configuration release --arch x86_64
-zip ../flowtoolz.github.io/codeface/lspservice/binaries/x86_64-apple-macosx/LSPService.zip .build/x86_64-apple-macosx/release/LSPService
+zip -j ../flowtoolz.github.io/codeface/lspservice/binaries/x86_64-apple-macosx/LSPService.zip .build/x86_64-apple-macosx/release/LSPService
