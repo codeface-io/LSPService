4d9eddab5c8fe98a006640979b442c9a8ce2f222
Sebastian Fichtner <contact@flowtoolz.com>
2022-09-25 23:12:07 +0200

    Update deployment script

--- build_releases.sh
+++ build_releases.sh
@@ -1,6 +1,6 @@
 swift build --configuration release --arch arm64
-zip -j ../flowtoolz.github.io/codeface/lspservice/binaries/arm64-apple-macosx/LSPService.zip .build/arm64-apple-macosx/release/LSPService
+zip -j ../codeface-io.github.io/lspservice/binaries/arm64-apple-macosx/LSPService.zip .build/arm64-apple-macosx/release/LSPService
 
 
 swift build --configuration release --arch x86_64
-zip -j ../flowtoolz.github.io/codeface/lspservice/binaries/x86_64-apple-macosx/LSPService.zip .build/x86_64-apple-macosx/release/LSPService
+zip -j ../codeface-io.github.io/lspservice/binaries/x86_64-apple-macosx/LSPService.zip .build/x86_64-apple-macosx/release/LSPService
