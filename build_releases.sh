737b706318675955cc009f036a486f8d098de745
Sebastian Fichtner <sebastian@codeface.io>
2022-12-03 04:08:23 +0100

    Edit release build script

--- build_releases.sh
+++ build_releases.sh
@@ -1,6 +1,7 @@
+#! /bin/zsh
+
 swift build --configuration release --arch arm64
 zip -j ../codeface-io.github.io/lspservice/binaries/arm64-apple-macosx/LSPService.zip .build/arm64-apple-macosx/release/LSPService
-
 
 swift build --configuration release --arch x86_64
 zip -j ../codeface-io.github.io/lspservice/binaries/x86_64-apple-macosx/LSPService.zip .build/x86_64-apple-macosx/release/LSPService
